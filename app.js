749e456a26e7374a0e01b3701e5977b0161d7648
MohammedZakhbat <zakhbat.med@gmail.com>
2026-05-18 11:15:38 +0200

    add proyecto

---
+++ app.js
@@ -0,0 +1,28 @@
+function resultados() {
+
+
+  document.getElementById("title1").textContent = "Tu Orden de prioridades :";
+  document.getElementById("title2").textContent = "Como ves la vida :";
+  document.getElementById("title3").textContent = "Tu gente:";
+
+
+  document.getElementById("label-vaca").textContent = "Tu Carrera";
+  document.getElementById("label-tigre").textContent = "El Orgullo";
+  document.getElementById("label-oveja").textContent = "El Amor";
+  document.getElementById("label-caballo").textContent = "La familia";
+  document.getElementById("label-mono").textContent = "El Dinero";
+
+ 
+  document.getElementById("label-perro").textContent = "Yo Soy...";
+  document.getElementById("label-gato").textContent = "Mi Pareja";
+  document.getElementById("label-rata").textContent = "Lo que más odio";
+  document.getElementById("label-cafe").textContent = "Para mí el sexo es";
+  document.getElementById("label-mar").textContent = "Y la vida...";
+
+
+  document.getElementById("label-amarillo").textContent = "Nunca te olvidará";
+  document.getElementById("label-naranja").textContent = "Puede ser tu gran amigo/a";
+  document.getElementById("label-blanco").textContent = "Tu alma gemela";
+  document.getElementById("label-rojo").textContent = "Realmente amas a...";
+  document.getElementById("label-verde").textContent = "Siempre estará en tu memoria";
+}
